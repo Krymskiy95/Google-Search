a7934c7584c7d330cf839a39c8cc219879928fb6
suprunov <rocknroll2418@gmail.com>
2021-10-25 10:53:44 +0300

    finished project

--- src/contexts/ResultContextProvider.js
+++ src/contexts/ResultContextProvider.js
@@ -1,27 +1,36 @@
 import React, { useContext, useState, createContext } from "react";
 
+
 const ResultContext = createContext()
-const baseUrl = 'https://google-search1.p.rapidapi.com/google-search'
+const baseUrl = 'https://google-search3.p.rapidapi.com/api/v1'
 
 export const ResultContextProvider = ({ children }) => {
     const [results, setResults] = useState([])
     const [isLoading, setIsLoading] = useState(false)
-    const [searchTerm, setSearchTerm] = useState('')
+    const [searchTerm, setSearchTerm] = useState('Elon Musk')
 
     const getResults = async (type) => {
         setIsLoading(true)
 
-        const responce = await fetch(`${baseUrl}${type}`, {
+        const res = await fetch(`${baseUrl}${type}`, {
             method: 'GET',
             headers: {
-                'x-rapidapi-host': 'google-search1.p.rapidapi.com',
-                'x-rapidapi-key': '3bd50207bbmshd336dda4055792fp1e73dajsna6ed5bbfc45c'
+                'x-user-agent': 'desktop',
+                'x-rapidapi-host': 'google-search3.p.rapidapi.com',
+                'x-rapidapi-key': process.env.REACT_APP_API_KEY
             }
         })
 
-        const data = await responce.json()
+        const data = await res.json()
 
-        setResults(data)
+        if (type.includes('/news')) {
+            setResults(data.entries)
+        } else if (type.includes('/images')) {
+            setResults(data.image_results)
+        } else {
+            setResults(data.results)
+        }
+
         setIsLoading(false )
     }
     return (
@@ -31,6 +40,4 @@ export const ResultContextProvider = ({ children }) => {
     )
 }
 
-export const useResultContext = () => {
-    useContext(ResultContext)
-}
+export const useResultContext = () => useContext(ResultContext)
